aed20fd5ca583475aad61540849d0e967637816b
Aloisio Keulbeck <122841738+aloisiok1@users.noreply.github.com>
2023-08-08 21:26:12 -0300

    08/08/23-2

--- Time Series/o_problema_de_time_series.py
+++ Time Series/o_problema_de_time_series.py
@@ -144,3 +144,33 @@ df1.head()
 
 df1.info()
 
+"""#PROPHET - Meta --> analise de time series
+https://facebook.github.io/prophet/docs/quick_start.html#python-api
+"""
+
+# Python
+import pandas as pd
+from prophet import Prophet
+
+df = pd.read_csv('https://raw.githubusercontent.com/facebook/prophet/main/examples/example_wp_log_peyton_manning.csv')
+df.head()
+
+m = Prophet()
+m.fit(df)
+
+future = m.make_future_dataframe(periods=365)
+future.tail()
+
+forecast = m.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+fig1 = m.plot(forecast)
+
+fig2 = m.plot_components(forecast)
+
+from prophet.plot import plot_plotly, plot_components_plotly
+
+plot_plotly(m, forecast)
+
+plot_components_plotly(m, forecast)
+
